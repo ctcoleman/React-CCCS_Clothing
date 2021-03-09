1c32041ff84fe988068cea63b3d7f2c1387de86e
ctcoleman <ctcoleman510@gmail.com>
2021-03-09 00:38:41 -0500

    added dummy data and images

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import MenuItem from '../menu-item/menu-item.component'
-
+import SECTIONS_DATA from '../../data/sections.data'
 import './directory-menu.style.sass'
 
 export default class DirectoryMenu extends Component {
@@ -8,40 +8,7 @@ export default class DirectoryMenu extends Component {
     super() 
 
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens',
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+      sections: SECTIONS_DATA
     }
   }
 
@@ -49,8 +16,9 @@ export default class DirectoryMenu extends Component {
     return(
       <div className='directory-menu'>
        {
-        this.state.sections.map(({id, ...otherProps}) => <MenuItem key={id} {...otherProps} />)
-        } 
+        this.state.sections.map(({id, ...otherProps}) => 
+          <MenuItem key={id} {...otherProps} />
+        )}
       </div>
       
     )
---
+++ src/data/sections.data.js
@@ -0,0 +1,44 @@
+/* Used as starting data for Directory */
+
+const SECTIONS_DATA = [
+    {
+        title: 'hats',
+        // imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        imageUrl: '/img/homepage/hats.png', // local file in public/img/homepage/
+        id: 1,
+        linkUrl: 'shop/hats'
+
+    },
+    {
+        title: 'jackets',
+        // imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        imageUrl: '/img/homepage/jackets.png', // local file in public/img/homepage/
+        id: 2,
+        linkUrl: 'shop/jackets'
+    },
+    {
+        title: 'sneakers',
+        // imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        imageUrl: '/img/homepage/sneakers.png', // local file in public/img/homepage/
+        id: 3,
+        linkUrl: 'shop/sneakers'
+    },
+    {
+        title: 'womens',
+        // imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        imageUrl: '/img/homepage/womens.png', // local file in public/img/homepage/
+        size: 'large',
+        id: 4,
+        linkUrl: 'shop/womens'
+    },
+    {
+        title: 'mens',
+        // imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        imageUrl: '/img/homepage/men.png', // local file in public/img/homepage/
+        size: 'large',
+        id: 5,
+        linkUrl: 'shop/mens'
+    }
+];
+
+export default SECTIONS_DATA;
