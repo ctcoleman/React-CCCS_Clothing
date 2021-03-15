668758b62e0e7c067c7fb0ca06753274dd8e2187
ctcoleman <ctcoleman510@gmail.com>
2021-03-14 20:48:12 -0400

    Fixed google sign in button causing error border for needed for details

--- src/components/form-button/form-button.component.jsx
+++ src/components/form-button/form-button.component.jsx
@@ -1,7 +1,7 @@
 import './form-button.styles.sass'
 
 const FormButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} form-button`} {...otherProps}>
+  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} form-button`} {...otherProps} type="button">
     {children}
   </button>
 )
