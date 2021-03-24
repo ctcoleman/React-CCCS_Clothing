1d768cee5b20e1692129ef3a7c4bdf91b9a818c8
ctcoleman <ctcoleman510@gmail.com>
2021-03-24 18:40:40 -0400

    removed duplicate additions to cart-items array and added quantity value

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,30 @@
+import { connect } from 'react-redux'
+import FormButton from '../form-button/form-button.component'
+import { addItem } from '../../redux/cart/cart.actions'
 import './collection-item.style.sass'
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{ name }</span>
-      <span className="price">{ price }</span>
-    </div>
-  </div>
-)
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
 
-export default CollectionItem
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{ name }</span>
+        <span className="price">{ price }</span>
+      </div>
+      <FormButton onClick={() => addItem(item)} inverted>Add to Cart</FormButton>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/components/form-button/form-button.component.jsx
+++ src/components/form-button/form-button.component.jsx
@@ -1,7 +1,11 @@
 import './form-button.styles.sass'
 
-const FormButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} form-button`} {...otherProps} type="button">
+const FormButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`
+    ${inverted ? 'inverted' : ''}
+    ${isGoogleSignIn ? 'google-sign-in' : ''}form-button`}
+    {...otherProps}
+  >
     {children}
   </button>
 )
